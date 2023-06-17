a8a677b669f1c1fdafe40f575ede78de6c64930e
Stolarek <przemyslaw.stolarek1@gmail.com>
2023-06-17 10:06:46 +0200

    implement color picker

--- mandalian/src/components/DrawingCanvas/DrawingCanvas.tsx
+++ mandalian/src/components/DrawingCanvas/DrawingCanvas.tsx
@@ -1,9 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { ChromePicker, ColorResult } from 'react-color';
 
 const DrawingCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [isDrawing, setIsDrawing] = useState<boolean>(false);
   const [prevPosition, setPrevPosition] = useState({ x: 0, y: 0 });
+  const [color, setColor] = useState<string>('#000');
+
   const width = 500;
   const height = 500;
 
@@ -16,7 +19,7 @@ const DrawingCanvas: React.FC = () => {
       setIsDrawing(true);
       setPrevPosition({ x: e.clientX, y: e.clientY });
     };
-
+    context.strokeStyle = color;
     const handleMouseMove = (e: MouseEvent) => {
       if (isDrawing) {
         const prevPos = {
@@ -99,7 +102,7 @@ const DrawingCanvas: React.FC = () => {
       document.removeEventListener('mouseout', handleMouseOut);
       document.addEventListener('mouseup', handleMouseUp);
     };
-  }, [isDrawing, prevPosition]);
+  }, [isDrawing, prevPosition, color]);
   const [backgroundImage, setBackgroundImage] = useState<null | string>(null);
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
@@ -114,13 +117,13 @@ const DrawingCanvas: React.FC = () => {
     }
   };
 
+  const handleColor = (newColor: ColorResult) => setColor(newColor.hex);
   return (
     <div>
       <h1>Create Mandalas</h1>
       <div onDrop={handleDrop} onDragOver={e => e.preventDefault()}>
         <p>Drag and drop an image here</p>
       </div>
-
       <canvas
         ref={canvasRef}
         width={width}
@@ -130,6 +133,7 @@ const DrawingCanvas: React.FC = () => {
           background: `url(${backgroundImage})`,
         }}
       />
+      <ChromePicker color={color} onChange={handleColor} disableAlpha={true} />
     </div>
   );
 };
